5e6bdaa9a9d3614a91f7318fe8e048b9d27a75ef
Maria Roberta Prado <75685022+mariarobertap@users.noreply.github.com>
2022-03-25 14:14:19 -0300

    Update first_and_last.py

--- Array/CodingQuestions/first_and_last.py
+++ Array/CodingQuestions/first_and_last.py
@@ -1,28 +1,41 @@
-'''
-Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+#. 34. Find First and Last Position of Element in Sorted Array
 
-If target is not found in the array, return [-1, -1].
+#Link: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 
-You must write an algorithm with O(log n) runtime complexity.
-https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+#Note: store the first time and last
 
-'''
+#Big O notation: Time O(n): Memory: O(1)
 
+#######################################
 
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        firstPosition = None
+        lastPosition = None
 
-
-def searchRange(self, nums, target):
-
-    firstPosition = None
-    lastPosition = None
-
-    for i in range(len(nums)):
-        if(nums[i] == target and firstPosition == None):
-            firstPosition = i
-        if(nums[i] == target and i > lastPosition):
-            lastPosition = i
+        for i in range(len(nums)):
             
-    if(firstPosition == None and lastPosition == None):
-        return [-1,-1]
-
-    return [firstPosition, lastPosition]
+            if(nums[i] == target and firstPosition == None):
+                firstPosition = i
+            if(nums[i] == target and i > lastPosition):
+                lastPosition = i
+                
+        
+        if(firstPosition == None and lastPosition == None):
+            return [-1,-1]
+      
+        
+        return [firstPosition, lastPosition]
+                
+        
+        
+        
+        
+        
+        
