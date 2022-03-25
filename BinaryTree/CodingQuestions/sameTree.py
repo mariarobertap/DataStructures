10b7e08e9b27d0e84be8b05515bad93c133ea1d5
Maria Roberta Prado <75685022+mariarobertap@users.noreply.github.com>
2022-03-24 22:38:30 -0300

    Create sameTree.py

---
+++ BinaryTree/CodingQuestions/sameTree.py
@@ -0,0 +1,38 @@
+#100. Same Tree - LeetCode
+
+#Link: https://leetcode.com/problems/same-tree/
+
+#Note: Use DSF algo with a recursive solution to compare all the nodes
+
+#Big O notation: Time O(n): Memory: O(1)
+
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        
+        
+        if(not p and not q):
+            return True
+        
+        if(not p or not q):
+            return False
+        
+        if(p.val != q.val):
+            return False
+        
+        
+        return(self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right) )
+        
+        
