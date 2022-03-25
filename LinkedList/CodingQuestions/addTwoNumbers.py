741092d05e73de7f9c575a860d2c0c48499250c7
Maria Roberta Prado <75685022+mariarobertap@users.noreply.github.com>
2022-03-25 13:33:48 -0300

    Update and rename AddTwoNumbers.py to addTwoNumbers.py

--- LinkedList/CodingQuestions/addTwoNumbers.py
+++ LinkedList/CodingQuestions/addTwoNumbers.py
@@ -1,27 +1,17 @@
+#2. Add Two Numbers
+
+#Link: https://leetcode.com/problems/add-two-numbers/
+
+#Note: 
+
+#Big O notation: Time O(n): Memory: O(1)
+#######################################
+
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-'''
-
-URL  = https://leetcode.com/problems/add-two-numbers/
-
-
-You are given two non-empty linked lists representing two non-negative integers.
-The digits are stored in reverse order, and each of their nodes contains a single digit.
-Add the two numbers and return the sum as a linked list.
-
-You may assume the two numbers do not contain any leading zero, except the number 0 itself.
-
-
-Input: l1 = [2,4,3], l2 = [5,6,4]
-Output: [7,0,8]
-Explanation: 342 + 465 = 807.
-
-
-'''
-
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
         """
